2be0c20d3181e6beae26c5d0fa94af9a2950b3df
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-09-09 20:17:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  until board = ["X","X","X","X","X","X","X","X","O"]
+  while board = ["X","X","X","X","X","X","X","X","X"]
   turn(board)
 end
 end
